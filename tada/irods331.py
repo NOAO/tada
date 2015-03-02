a5083b5f122a7fdc2657ba46c14655151c7c99f9
Steve Pothier <pothier@noao.edu>
2015-03-02 10:48:28 -0700

    snap

--- tada/irods331.py
+++ tada/irods331.py
@@ -22,10 +22,14 @@ def irods_put331(local_fname, irods_fname):
     "Copy local_fname to irods_fname, creating dirs if needed."
     logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
     logging.debug('   irods_put331 env:{})'.format(os.environ))
-    icmdpath = '/usr/local/share/applications/irods3.3.1/iRODS/clients/icommands/bin'
-    cmd1 = '"{}/imkdir -p {}"'.format(icmdpath, os.path.dirname(irods_fname))
+    icmdpath = ('/usr/local/share/applications/irods3.3.1/iRODS/clients'
+                '/icommands/bin')
+    #!cmd1 = '"{}/imkdir -p {}"'.format(icmdpath, os.path.dirname(irods_fname))
     try:
-        subprocess.check_output(['sudo', 'su', '-', 'tada', '-c', cmd1])
+        #!subprocess.check_output(['sudo', 'su', '-', 'tada', '-c', cmd1])
+        subprocess.check_output([os.path.join(icmdpath, 'imkdir'),
+                                  '-p',
+                                  os.path.dirname(irods_fname)])
         subprocess.check_output([os.path.join(icmdpath, 'iput'),
                                  '-f', '-K', local_fname, irods_fname])
 
