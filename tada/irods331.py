4a1621c9b4c0410e74e5881340451277ec0d7154
Steve Pothier <pothier@noao.edu>
2015-03-02 09:18:02 -0700

    snap

--- tada/irods331.py
+++ tada/irods331.py
@@ -1,38 +1,36 @@
+"All use of iRODS by TADA is done through these functions"
+
 import subprocess
 import logging
 import os
 
-def OLD_irods_put331(local_fname, irods_fname):
-    logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
-    
-    try:
-        subprocess.check_output(
-            ['/sandbox/tada/scripts/iput331 {} {}'
-             .format(local_fname, irods_fname)],
-            stderr=subprocess.STDOUT,
-            shell=True)
-    except subprocess.CalledProcessError as ex:
-        logging.error('Execution failed: {}; {}'
-                      .format(ex, ex.output.decode('utf-8')))
-        raise
-    
+#! def OLD_irods_put331(local_fname, irods_fname):
+#!     logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
+#!     
+#!     try:
+#!         subprocess.check_output(
+#!             ['/sandbox/tada/scripts/iput331 {} {}'
+#!              .format(local_fname, irods_fname)],
+#!             stderr=subprocess.STDOUT,
+#!             shell=True)
+#!     except subprocess.CalledProcessError as ex:
+#!         logging.error('Execution failed: {}; {}'
+#!                       .format(ex, ex.output.decode('utf-8')))
+#!         raise
+
 def irods_put331(local_fname, irods_fname):
+    "Copy local_fname to irods_fname, creating dirs if needed."
     logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
     logging.debug('   irods_put331 env:{})'.format(os.environ))
-    #! os.environ.copy()
-    #!env331 = dict(
-    #!    irodsEnvFile = '/home/tada/.irods/.irodsEnv'        
-    #!    )
-    icmdpath='/usr/local/share/applications/irods3.3.1/iRODS/clients/icommands/bin'
-    
+    icmdpath = '/usr/local/share/applications/irods3.3.1/iRODS/clients/icommands/bin'
     try:
-        subprocess.check_output(['{}/imkdir'.format(icmdpath),
-                                 '-p',  os.path.dirname(irods_fname)])
-        subprocess.check_output(['{}/iput'.format(icmdpath),
+        subprocess.check_output([os.path.join(icmdpath, 'imkdir'),
+                                 '-p', os.path.dirname(irods_fname)],
+                                shell=True)
+        subprocess.check_output([os.path.join(icmdpath, 'iput'),
                                  '-f', '-K', local_fname, irods_fname])
 
     except subprocess.CalledProcessError as ex:
         logging.error('Execution failed: {}; {}'
                       .format(ex, ex.output.decode('utf-8')))
         raise
-    
