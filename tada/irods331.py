4042a85a4227de50cc437a6c22a82d5ecf180f7b
Steve Pothier <pothier@noao.edu>
2015-03-03 08:43:47 -0700

    cleanup

--- tada/irods331.py
+++ tada/irods331.py
@@ -6,7 +6,7 @@ import os
 
 #! def OLD_irods_put331(local_fname, irods_fname):
 #!     logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
-#!     
+#!
 #!     try:
 #!         subprocess.check_output(
 #!             ['/sandbox/tada/scripts/iput331 {} {}'
@@ -21,20 +21,16 @@ import os
 def irods_put331(local_fname, irods_fname):
     "Copy local_fname to irods_fname, creating dirs if needed."
     logging.debug('irods_put331({}, {})'.format(local_fname, irods_fname))
-    logging.debug('   irods_put331 env:{})'.format(os.environ))
+    #! logging.debug('   irods_put331 env:{})'.format(os.environ))
     icmdpath = ('/usr/local/share/applications/irods3.3.1/iRODS/clients'
                 '/icommands/bin')
-    #!cmd1 = '"{}/imkdir -p {}"'.format(icmdpath, os.path.dirname(irods_fname))
     try:
-        #!subprocess.check_output(['sudo', 'su', '-', 'tada', '-c', cmd1])
         subprocess.check_output([os.path.join(icmdpath, 'imkdir'),
-                                  '-p',
-                                  os.path.dirname(irods_fname)],
-                                start_new_session=True,
-                            )
+                                 '-p',
+                                 os.path.dirname(irods_fname)])
+                                #! start_new_session=True)
         subprocess.check_output([os.path.join(icmdpath, 'iput'),
                                  '-f', '-K', local_fname, irods_fname])
-
     except subprocess.CalledProcessError as ex:
         logging.error('Execution failed: {}; {}'
                       .format(ex, ex.output.decode('utf-8')))
