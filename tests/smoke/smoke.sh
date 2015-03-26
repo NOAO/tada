9efa50d0303cbaa4494ce8eb3d9f71e3595f2516
Steve Pothier <pothier@noao.edu>
2015-03-26 12:23:00 -0700

    remove use of tada specific socket; better smoke - passes; too much diagnostics now

--- tests/smoke/smoke.sh
+++ tests/smoke/smoke.sh
@@ -27,43 +27,79 @@ origdir=`pwd`
 cd $dir
 
 PATH=$tadadir/scripts:$SCRIPTDIR:$PATH
+deletemirror=`type -path delete-mirror.sh`
+deletenoarch=`type -path delete-noarchive.sh`
 
 source smoke-lib.sh
 return_code=0
 SMOKEOUT="README-smoke-results.txt"
 
+function cleanStart () {
+    # Clear old transfer queue
+    dqcli --clear -s 
+
+    echo "yes" | sudo $deletemirror > /dev/null
+    echo "yes" | sudo $deletenoarch > /dev/null
+}
 
 echo ""
 echo "Starting tests in \"$dir\" ..."
 echo ""
 echo ""
 
-# Clear old transfer queue
-dqcli --clear 
-
-prms="-c -t 15"
+wait=50  # seconds to wait for file to make it thru ingest
+prms="-c -t $wait"
 MANIFEST=/var/log/tada/submit.manifest
 
-files="$tdata/dont-panic.jpg /data/bok/bokrm.20140425.0119.fits"
-stat=no-opt.log
-testCommand tada1_1 "tada-submit $prms $files 2>&1" "^\#" y
-awk '{ sub(".*/","",$3); print $2, $3, $5 } ' < $MANIFEST > $stat.clean
-testOutput tada1_2 $stat.clean '^\#' n
-#!testCommand tada1_3 "dqcli -s 2>&1" "^\#" n
 
-# lp -d astro -o _DTCALDAT=2014-09-21 /data/raw/nhs_2014_n14_299403.fits
-files="/data/raw/nhs_2014_n14_299403.fits"
-stat=opt1.log
-opt1="-o _DTCALDAT=2014-09-21"
-testCommand tada2_1 "tada-submit $opt1 $prms $files 2>&1" "^\#" y
-awk '{ sub(".*/","",$3); print $2, $3, $5 } ' < $MANIFEST > $stat.clean
-testOutput tada2_2 $stat.clean '^\#' n
-#!testCommand tada2_3 "dqcli -s 2>&1" "^\#" n
+##########################
+# 1_1: pass; non-FITS
+file=/data/other/uofa-mandle.jpg
+status=`basename $file`.status
+findout=find-`basename $file`.out
+cleanStart  > /dev/null
+testCommand tada1_1 "tada-submit $prms $file 2>&1" "^\#" y
+#! echo "MANIFEST:"; cat /var/log/tada/submit.manifest
+awk '{ sub(".*/","",$3); print $2, $3, $5 } ' < $MANIFEST > $status.clean
+testOutput tada1_2 $status.clean '^\#' n
+testCommand tada1_3 "dqcli -s 2>&1" "^\#" n
+find /var/tada -type f | sed 's|/[0-9]\+/|/|g' | sort > $findout
+testOutput tada1_4 $findout '^\#' n
+
+##########################
+# 2_1: pass ingest using options
+# lp -d astro -o _DTCALDAT=2014-09-21 /data/raw/nhs_2014_n14_299403.fitse
+file=/data/raw/nhs_2014_n14_299403.fits
+opt="-o _DTCALDAT=2014-09-21"
+status=`basename $file`.status
+findout=find-`basename $file`.out
+cleanStart  > /dev/null
+testCommand tada2_1 "tada-submit $opt $prms $file 2>&1" "^\#" y
+awk '{ sub(".*/","",$3); print $2, $3, $5 } ' < $MANIFEST > $status.clean
+testOutput tada2_2 $status.clean '^\#' n
+testCommand tada2_3 "dqcli -s 2>&1" "^\#" n
+find /var/tada -type f | sed 's|/[0-9]\+/|/|g' | sort > $findout
+testOutput tada2_4 $findout '^\#' n
+
+
+##########################
+# 3_1: fail ingest
+file=/data/bok/bokrm.20140425.0119.fits
+status=`basename $file`.status
+findout=find-`basename $file`.out
+cleanStart  > /dev/null
+testCommand tada3_1 "tada-submit $prms $file 2>&1" "^\#" y
+awk '{ sub(".*/","",$3); print $2, $3, $5 } ' < $MANIFEST > $status.clean
+testOutput tada3_2 $status.clean '^\#' n
+testCommand tada3_3 "dqcli -s 2>&1" "^\#" n
+find /var/tada -type f | sed 's|/[0-9]\+/|/|g' | sort > $findout
+testOutput tada3_4 $findout '^\#' n
+
 
 ###########################################
-#! echo "WARNING: ignoring remainder of tests"
-#! exit $return_code
-###########################################
+#!echo "WARNING: ignoring remainder of tests"
+#!exit $return_code
+###########################################a
 
 
 ##############################################################################
