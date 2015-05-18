71f25970dd1f8c1f5946724d961adb9b7d931a1a
Steve Pothier <pothier@noao.edu>
2015-05-18 14:53:14 -0700

    all 4 smokes enabled and pass (16 tests)

--- tada/ingest_decoder.py
+++ tada/ingest_decoder.py
@@ -8,12 +8,24 @@ import re
 exists_re = re.compile(r"has already been stored in the database")
                        
 
+# "Failure reason:Failed to ingest file:/noao-tuc-z1/tada/vagrant/23/ksb_041016_195304_uuu_1188439357.hdr error msg:Could not find unique proposal TEST-noao in metadata database, found 0"
+nonunique_propid_re = re.compile(r"Could not find unique proposal")
+
+# "Failure reason:Failed to ingest file:/noao-tuc-z1/tada/vagrant/2/k4k_140922_234549_zuu_1186823651.hdr error msg:Got more than one observation matching calibration date for proposal. Query: select distinct o from ObservationEntity o join fetch o.proposalSet p where p.proposalId = ?1 and o.calibrationDate between ?2 and ?3 and o.publicDataReleaseDate < ?4"
+dup_obs_prop_re = re.compile(r"Got more than one observation matching calibration date for proposal")
+
 def decodeIngest(response):
     if exists_re.search(response):
         msg = ('Already in DB. To submit this file, '
                'remove existing from DB and do: "dqcli --redo"')
+    elif nonunique_propid_re.search(response):
+        msg = ('Bad DTPROPID. To submit this file, '
+               'fix header or resubmit with "-o _DTPROPID=<propid>"')
+    elif dup_obs_prop_re.search(response):
+        msg = ('Duplicate DTPROPID, DTCALDAT combination. To submit this file, '
+               'fix header or resubmit with "-o _DTPROPID=<propid> -o _DTCALDAT=<caldat>"')
     else:
-        msg = '<none>'
+        msg = '<no operator message in ingest_decoder.py::decodIngest()>'
 
     return 'Operator:: ' + msg
 
