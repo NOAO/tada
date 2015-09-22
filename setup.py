99acdf939fd06ba9fde3819603673fef19b15a41
Steve Pothier <pothiers@gmail.com>
2015-09-22 13:50:47 -0700

    snap

--- setup.py
+++ setup.py
@@ -100,9 +100,10 @@ setup(
     entry_points={
         'console_scripts': [
             #! 'mockingest=tada.mock_ingest:main',
+            #! 'prep_fits_for_ingest=tada.prep_fits_for_ingest:main',
             'fits_compliant=tada.fits_utils:main',
             'extract_fits_header=tada.fits_utils:extract_header',
-            #! 'prep_fits_for_ingest=tada.prep_fits_for_ingest:main',
+            'direct_submit=tada.submit:main',
         ],
     },
 )
