e466c82a96b6254460578b9b8d257b82fc481d3a
Steve Pothier <pothier@noao.edu>
2015-10-26 08:13:12 -0700

    smoke.scrape.sh passes 11/11 (need tests for 5 more instruments)

---
+++ tests/smoke/README-smoke-results.scrape.txt
@@ -0,0 +1,1 @@
+All 11 tests passed on  Mon Oct 26 15:07:28 UTC 2015
--- tests/smoke/smoke.scrape.sh
+++ tests/smoke/smoke.scrape.sh
@@ -8,8 +8,8 @@ SCRIPTDIR=$(dirname $SCRIPT) #Absolute path this script is in
 testdir=$(dirname $SCRIPTDIR)
 tadadir=$(dirname $testdir)
 # tadadir=/sandbox/tada
-tdata=$SCRIPTDIR/data
-# tdata=/sandbox/tada/tests/smoke/data
+tdata=$SCRIPTDIR/data-scrape
+# tdata=/sandbox/tada/tests/smoke/data-scrape
 
 echo "tdata=$tdata; tadadir=$tadadir; SCRIPTDIR=$SCRIPTDIR"
 
@@ -21,7 +21,7 @@ export PATH=$tadadir/../tada-tools/dev-scripts:$SCRIPTDIR:$PATH
 
 source smoke-lib.sh
 return_code=0
-SMOKEOUT="README-smoke-results.direct.txt"
+SMOKEOUT="README-smoke-results.scrape.txt"
 
 echo ""
 echo "Starting tests in \"$SCRIPT\" ..."
@@ -35,29 +35,15 @@ fi
 echo ""
 
 ###########################################
-### fits_compliant
-###
-
-## bad DATE-OBS content
-testCommand fc1_1 "fits_compliant --header $tdata/kp109391.fits.fz" "^\#" n
-
-# missing raw
-testCommand fc2_1 "fits_compliant $tdata/kptest.fits" "^\#" n
-
-###########################################
 ### fits_submit
 ###
-function fsubmit () {
-    ffile=$1; shift
-    pers=""
-    for p; do
-	    pers="$pers -p $p"
-    done
-    #!fits_submit -p smoke $pers $ffile 2>&1 | perl -pe 's|as /noao-tuc-z1/.*||'
-    msg=`fits_submit -p smoke $pers $ffile 2>&1`
+function fsub () {
+    pers=$1
+    ffile=$2
+
+    msg=`fits_submit -p smoke -p $pers $ffile 2>&1`
     status=$?
     if [ $status -eq 0 ]; then
-        # e.g. msg="SUCCESS: archived /sandbox/tada/tests/smoke/data/obj_355.fits as /noao-tuc-z1/mtn/20141219/WIYN/2012B-0500/uuuu_141220_130138_uuu_TADATEST_2417885023.fits"
         irodsfile=`echo $msg | cut -s --delimiter=' ' --fields=5`
         archfile=`basename $irodsfile`
         echo $msg 2>&1 | perl -pe 's|as /noao-tuc-z1/.*||'
@@ -65,8 +51,6 @@ function fsubmit () {
         echo ""
         echo "Successful ingest. Added $archfile to PROVISIONAL list via ws"
     else
-        #! >&2 echo "EXECUTED: fits_submit -p smoke $pers $ffile"  
-        #! >&2 echo $msg
         echo "EXECUTED: fits_submit -p smoke $pers $ffile"  
         echo $msg
     fi
@@ -74,42 +58,19 @@ function fsubmit () {
 }
 
 
-## non-FITS; (reject, not try to ingest)
-testCommand fs1_1 "fsubmit $tdata/uofa-mandle.jpg" "^\#" n 1
 
-## compliant FITS with no options (no need for them, so ingest success)
-testCommand fs2_1 "fsubmit $tdata/k4k_140922_234607_zri.fits.fz" "^\#" n
-
-## compliant FITS with no options (BUT, already inserted above so ingest FAIL)
-testCommand fs2b_1 "fsubmit $tdata/k4k_140922_234607_zri.fits.fz" "^\#" n 2
-
-## bad format for DATE-OBS
-testCommand fs3_1 "fsubmit $tdata/kp109391.fits.fz" "^\#"  n 2
-
-## FITS made compliant via passed personality options; compress on-the-fly
-## (ingest success)
-testCommand fs4_1 "fsubmit $tdata/obj_355.fits wiyn-whirc" "^\#" n
-
-## FITS made compliant via passed personality options; multi-extensions
-## (ingest success)
-testCommand fs5_1 "fsubmit $tdata/obj_355.fits.fz wiyn-whirc" "^\#" n
-
-## non-compliant FITS, missing RAW (ingest failure)
-testCommand fs6_1 "fsubmit $tdata/kptest.fits" "^\#" n 1
-
-###########################################
-### pipeline_submit
-###
-function psubmit () {
-    ffile=$1; shift
-    #msg=`pipeline_submit $ffile 2>&1`
-    #!status=$?
-    pipeline_submit $ffile 2>&1 | perl -pe 's|as /noao-tuc-z1/.*||'
-}
-
-testCommand ps1_1 "psubmit $tdata/uofa-mandle.jpg" "^\#" n
-testCommand ps2_1 "psubmit $tdata/c4d_130901_031805_oow_g_d2.fits.fz" "^\#" n
-
+testCommand sc1_1 "fsub bok23m-90prime $tdata/bok23m-90prime/d7212.0062.fits.fz" "^\#" n
+testCommand sc2_1 "fsub ct13m-andicam $tdata/ct13m-andicam/ir141225.0179.fits.fz" "^\#" n
+testCommand sc3_1 "fsub ct15m-echelle $tdata/ct15m-echelle/chi150724.1000.fits.fz" "^\#" n
+testCommand sc4_1 "fsub ct4m-cosmos $tdata/ct4m-cosmos/n3.25523.fits.fz" "^\#" n
+#testCommand sc5_1 "fsub ct4m-decam  $tdata/ct4m-decam/list.txt" "^\#" n
+testCommand sc6_1 "fsub kp09m-hdi $tdata/kp09m-hdi/c7015t0267b00.fits.fz" "^\#" n
+testCommand sc7_1 "fsub kp4m-mosaic_1_1 $tdata/kp4m-mosaic_1_1/spw54553.fits.fz" "^\#" n
+testCommand sc8_1 "fsub kp4m-newfirm $tdata/kp4m-newfirm/nhs_2015_n04_319685.fits.fz" "^\#" n
+testCommand sc9_1 "fsub soar-goodman $tdata/soar-goodman/0079.spec_flat.fits.fz" "^\#" n
+testCommand sc10_1 "fsub soar-spartan $tdata/soar-spartan/011-6365d0.fits.fz" "^\#" n
+testCommand sc11_1 "fsub wiyn-bench $tdata/wiyn-bench/24dec_2014.061.fits.fz" "^\#" n
+testCommand sc12_1 "fsub wiyn-whirc $tdata/wiyn-whirc/obj_355.fits.fz" "^\#" n
 
 
 
