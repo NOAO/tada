6bb2c28c202edd144f080d7b640dd1c9652389f5
Steve Pothier <pothier@noao.edu>
2016-01-07 08:37:20 -0700

    handle cp and rsync to dropbox

--- tada/watch.py
+++ tada/watch.py
@@ -2,6 +2,11 @@
 """When files appear in watched directory, ingest them. This should be
 wrapped into a service."""
 
+'''
+TODO:
+- YAML for personality files
+'''
+
 import argparse
 import logging
 import os
@@ -10,49 +15,64 @@ import time
 import shutil
 from pathlib import PurePath
 
-#from . import submit as ts
-#import submit as ts
+import yaml
 
 import watchdog.events
 import watchdog.observers
 
+from . import submit as ts
+
+
 def fake_submit(ifname, checksum, qname, qcfg):
     pfail = 0.5
     r = random.random()
-    logging.debug('r={}'.format(r))
     if  r < pfail:
         raise Exception('FAILED')
     return ifname
 
-class DropEventHandler(watchdog.events.FileSystemEventHandler):
+def new_fits(ifname, watched_path, rejected_path, dest_path=None):
+    p = PurePath(ifname).relative_to(watched_path)
+    logging.debug('DBG: new_fits: ifname={}, p={}'
+                  .format(ifname, str(p)))
+    #if isinstance(event, watchdog.events.FileCreatedEvent):
+    if p.suffix == '.fz' or p.suffix == '.fits':
+        logging.debug('Got FITS: {}'.format(ifname))
+        destfname=str(rejected_path / p)
+        try:
+            #destfname = ts.submit_to_archive(ifname, checksum, 
+            #                                 qname, qcfg)
+            fake_submit(ifname, 'checksum', 'qname', 'qcfg')
+        except Exception as sex:
+            # FAILURE: stash it
+            logging.debug('Ingest FAILED: stash into: {}'
+                          .format(destfname))
+            shutil.move(ifname, destfname)
+        else:
+            # SUCCESS: remove it
+            logging.debug('Ingest SUCCEEDED: remove: {}'.format(ifname))
+            os.remove(ifname)
+    
+
+class SubmitEventHandler(watchdog.events.FileSystemEventHandler):
     def __init__(self, watched_dir, rejected_dir):
         self.watched_path = PurePath(watched_dir)
         self.rejected_path = PurePath(rejected_dir)
         super(watchdog.events.FileSystemEventHandler).__init__()
         
     def on_created(self,event):
-        if isinstance(event, watchdog.events.FileCreatedEvent):
-            ifname=event.src_path
-            p = PurePath(ifname).relative_to(self.watched_path)
-            destfname=str(self.rejected_path / p)
-            try:
-                #destfname = ts.submit_to_archive(ifname, checksum, qname, qcfg)
-                fake_submit(ifname, 'checksum', 'qname', 'qcfg')
-            except Exception as sex:
-                # FAILURE: stash it
-                logging.debug('Ingest FAILED: stash into: {}'.format(destfname))
-                shutil.move(event.src_path, destfname)
-            else:
-                # SUCCESS: remove it
-                logging.debug('Ingest SUCCEEDED: remove: {}'
-                              .format(event.src_path))
-                os.remove(ifname)
+        new_fits(event.src_path, self.watched_path, self.rejected_path)
+    def on_moved(self,event):
+        # for rsync: moved from tmp file to final filename
+        new_fits(event.dest_path, self.watched_path, self.rejected_path)
                 
 def ingest_drops(watched_dir, rejected_dir):
-    handler = DropEventHandler(watched_dir, rejected_dir)
+    handler = SubmitEventHandler(watched_dir, rejected_dir)
     observer = watchdog.observers.Observer()
+    logging.info('Watching directory: {}'.format(watched_dir))
     observer.schedule(handler, watched_dir, recursive=True)
+    logging.debug('dbg: starting')
     observer.start()
+    logging.debug('dbg: Started')
     try:
         while True:
             time.sleep(1)
