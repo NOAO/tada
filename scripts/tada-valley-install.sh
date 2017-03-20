8da63f65dd5f29952ffd31714a05d0845e3adfde
Steve Pothier <pothiers@gmail.com>
2017-03-20 07:01:06 -0700

    snap

--- scripts/tada-valley-install.sh
+++ scripts/tada-valley-install.sh
@@ -3,7 +3,10 @@
 # run as: tada
 # Used by puppet
 
+LOG="/etc/tada/install.log"
+date > $LOG
+
 cd /opt/tada
 source /opt/tada/venv/bin/activate
-/opt/tada/venv/bin/python3 setup.py install --force
-installTadaTables
+/opt/tada/venv/bin/python3 setup.py install --force >> $LOG
+installTadaTables >> $LOG
