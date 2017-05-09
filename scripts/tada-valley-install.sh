5bf0d67aced3e7c335b0ee5b6a15e4ece7516464
Steve Pothier <pothier@noao.edu>
2017-05-09 13:27:58 -0700

    add svc restart to tada-valley-install.sh can be used in dev

--- scripts/tada-valley-install.sh
+++ scripts/tada-valley-install.sh
@@ -17,4 +17,26 @@ echo "Running install on dir: $dir"
 python3 setup.py install --force >> $LOG
 installTadaTables                >> $LOG
 echo "Installed TADA version: $VERSION" >> $LOG
+
+sudo rm /var/log/tada/*.err
+sudo service dqd restart > /dev/null
+if [ -s /var/log/tada/dqd.err ]; then
+    cat /var/log/tada/dqd.err
+    echo "ERROR: The 'dqd' service is not running"
+    exit 1
+else
+    echo "dqd restarted successfully."
+    #!sudo dqcli --clear
+    dqcli --clear
+fi
+
+sudo service watchpushd restart > /dev/null
+if [ -s /var/log/tada/watchpushd.err ]; then
+    cat /var/log/tada/watchpushd.err
+    echo "ERROR: The 'watchpushd' service is not running"
+    exit 1
+else
+	echo "watchpushd restarted successfully"
+fi
+
 cat $LOG
